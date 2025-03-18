3b09397ba07d9d92dfbc1058dda4d546609922e5
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-03-18 10:50:21 +0000

    Update functions.cpp

--- Mechanics/Projectile Motion/functions.cpp
+++ Mechanics/Projectile Motion/functions.cpp
@@ -10,12 +10,10 @@ void time(double Vi, double rad, double tau){
 	const double g = 9.81;
 	
 	Vy = Vi * sin(rad);
-	
 	T = tau * log((Vy + (g * tau)) / (g * tau));
 	t = T / 2;
 	
 	cout << "\nThe time to reach the maximum height is " << t << "s\n";
-	
 	cout << "The time of flight is " << T << "s\n";
  }
  
@@ -26,9 +24,7 @@ void time(double Vi, double rad, double tau){
 	const double g = 9.81;
 	
 	Vy = Vi * sin(rad);
-	
 	T = tau * log((Vy + (g * tau)) / (g * tau));
-	
 	H = (((Vi * sin(rad)) + (g * tau)) * tau * (1-exp(-T/tau))) - (g * tau * T);
 	
 	cout << "The maximum height reached is " << H << "m\n";
@@ -41,7 +37,6 @@ void time(double Vi, double rad, double tau){
 
 	Vx = Vi * cos(rad);
 	Vy = Vi * sin(rad);
-	
 	Rvac = (2 * Vx * Vy) / g; 
 	R = Rvac * (1 - ((4 * Vy) / (3 * tau * g)));
  
