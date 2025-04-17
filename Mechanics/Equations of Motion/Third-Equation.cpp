f92742a3cd9e24cf7ec9482fbc229d38fd5103b6
Samuel Fiifi <algorithmicsamuelx@gmail.com>
2025-04-17 20:12:16 +0000

    Update Third-Equation.cpp

--- Mechanics/Equations of Motion/Third-Equation.cpp
+++ Mechanics/Equations of Motion/Third-Equation.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+void third_equation(double a, double b, double c, int d){
+	
+}
+
+int main() {
+    cout << " -------------------------------------------------------------------------------" << endl;
+    cout << "|                  Exploring Newton's Equations of Motion                      |" << endl;
+    cout << "|     Three fundamental and widely known equations in the world of Physics.    |" << endl;
+    cout << "|           Tackling the third equation of motion (s = ut + 0.5at^2)          |" << endl;
+    cout << " -------------------------------------------------------------------------------" << endl; 
+    
+  
+    
+} while(ans == 'y');
+
+    return 0;
+}
